720ab3d97e15805d676884f9ae9fc70e7371306f
Dan <trinitroglycerin@protonmail.com>
2022-12-01 22:19:58 -0800

    Day 2, Part 2

--- aoc/day2.rs
+++ aoc/day2.rs
@@ -4,15 +4,21 @@ use std::{io::Read, str::FromStr};
 use aoc2022::Part;
 
 pub(crate) fn execute<R: Read>(part: &aoc2022::Part, mut reader: R) -> aoc2022::Result<()> {
-    match part {
+    let mut raw = String::new();
+    reader.read_to_string(&mut raw)?;
+
+    let score = match part {
         Part::Part1 => {
-            let mut raw = String::new();
-            reader.read_to_string(&mut raw)?;
             let guide: StrategyGuide1 = raw.parse().unwrap();
-            println!("{}", guide.calculate_score());
+            guide.calculate_score()
         }
-        _ => unimplemented!(),
-    }
+        Part::Part2 => {
+            let guide: StrategyGuide2 = raw.parse().unwrap();
+            guide.calculate_score()
+        }
+    };
+
+    println!("{}", score);
     Ok(())
 }
 
@@ -20,11 +26,18 @@ pub(crate) fn execute<R: Read>(part: &aoc2022::Part, mut reader: R) -> aoc2022::
 enum StrategyGuideError {
     InvalidFormatError,
     UnknownActionError,
+    UnknownOutcomeError,
 }
 
 impl From<UnknownActionError> for StrategyGuideError {
     fn from(_: UnknownActionError) -> Self {
         Self::UnknownActionError
+    }
+}
+
+impl From<UnknownOutcomeError> for StrategyGuideError {
+    fn from(_: UnknownOutcomeError) -> Self {
+        Self::UnknownOutcomeError
     }
 }
 
@@ -43,11 +56,77 @@ struct StrategyGuide1 {
     actions: Vec<(Action, Action)>,
 }
 
+impl StrategyGuide1 {
+    fn new() -> Self {
+        Self {
+            actions: Vec::new(),
+        }
+    }
+}
+
 impl FromStr for StrategyGuide1 {
     type Err = StrategyGuideError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut guide = StrategyGuide1 {
+        let mut guide = StrategyGuide1::new();
+        for line in s.lines() {
+            let parts: Vec<&str> = line.split(" ").collect();
+            guide.actions.push((parts[0].parse()?, parts[1].parse()?));
+        }
+
+        Ok(guide)
+    }
+}
+
+fn calculate_action_for_desired_outcome(other: Action, desired_outcome: Outcome) -> Action {
+    match (other, desired_outcome) {
+        (Action::Rock, Outcome::Win) => Action::Paper,
+        (Action::Rock, Outcome::Loss) => Action::Scissors,
+
+        (Action::Scissors, Outcome::Win) => Action::Rock,
+        (Action::Scissors, Outcome::Loss) => Action::Paper,
+
+        (Action::Paper, Outcome::Win) => Action::Scissors,
+        (Action::Paper, Outcome::Loss) => Action::Rock,
+
+        (a, Outcome::Draw) => a,
+    }
+}
+
+/// In the second version of the strategy guide, the actions parsed are instead (Action, Outcome), where Action is what the opponent will play, and Outcome is the outcome we want to get.
+struct StrategyGuide2 {
+    actions: Vec<(Action, Outcome)>,
+}
+
+impl StrategyGuide2 {
+    fn new() -> Self {
+        Self {
+            actions: Vec::new(),
+        }
+    }
+
+    fn calculate_round_score(desired_outcome: Outcome, opponent: Action) -> u32 {
+        desired_outcome.u32()
+            + calculate_action_for_desired_outcome(opponent, desired_outcome).u32()
+    }
+
+    /// Calculates the score of each action in the strategy guide.
+    ///
+    /// The score is equal to a fixed amount for the shape you played (1 for Rock, 2 for Paper and 3 for Scissors), plus an additional amount based on the outcome of the round; 0 for a loss, 3 for a draw and 6 for a win.
+    fn calculate_score(&self) -> u32 {
+        self.actions
+            .iter()
+            .fold(0, |score, (opponent, desired_outcome)| {
+                score + Self::calculate_round_score(*desired_outcome, *opponent)
+            })
+    }
+}
+
+impl FromStr for StrategyGuide2 {
+    type Err = StrategyGuideError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut guide = StrategyGuide2 {
             actions: Vec::new(),
         };
 
@@ -103,12 +182,15 @@ impl FromStr for Action {
     }
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
 enum Outcome {
     Win = 6,
     Draw = 3,
     Loss = 0,
 }
+
+#[derive(Debug)]
+struct UnknownOutcomeError;
 
 impl Outcome {
     fn u32(&self) -> u32 {
@@ -116,9 +198,26 @@ impl Outcome {
     }
 }
 
+impl FromStr for Outcome {
+    type Err = UnknownOutcomeError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "X" => Ok(Outcome::Loss),
+            "Y" => Ok(Outcome::Draw),
+            "Z" => Ok(Outcome::Win),
+            _ => Err(Self::Err {}),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::day2::{determine_round_outcome, Action, Outcome, StrategyGuide1};
+    use crate::day2::{
+        calculate_action_for_desired_outcome, determine_round_outcome, Action, Outcome,
+        StrategyGuide1,
+    };
+
+    use super::StrategyGuide2;
 
     #[test]
     fn has_correct_scores() {
@@ -169,5 +268,32 @@ B X
 C Z";
         let guide: StrategyGuide1 = raw.parse().unwrap();
         assert_eq!(guide.calculate_score(), 15);
+    }
+
+    #[test]
+    fn correctly_determines_action_for_desired_outcome() {
+        assert_eq!(
+            Action::Scissors,
+            calculate_action_for_desired_outcome(Action::Paper, Outcome::Win)
+        );
+
+        assert_eq!(
+            Action::Scissors,
+            calculate_action_for_desired_outcome(Action::Scissors, Outcome::Draw)
+        );
+
+        assert_eq!(
+            Action::Scissors,
+            calculate_action_for_desired_outcome(Action::Rock, Outcome::Loss)
+        );
+    }
+
+    #[test]
+    fn v2_correctly_calculates_scores() {
+        let raw = "A Y
+B X
+C Z";
+        let guide: StrategyGuide2 = raw.parse().unwrap();
+        assert_eq!(guide.calculate_score(), 12);
     }
 }
