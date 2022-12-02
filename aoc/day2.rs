946530572e20608375ec851b7f4fe643da45e5ed
Dan <trinitroglycerin@protonmail.com>
2022-12-01 21:54:07 -0800

    Add tests that demonstrate our calculator works

--- aoc/day2.rs
+++ aoc/day2.rs
@@ -38,20 +38,46 @@ impl FromStr for StrategyGuide {
     }
 }
 
+/// Determines if  A won or lost the round based on the given actions.
+fn determine_round_outcome(a: Action, b: Action) -> Outcome {
+    match (a, b) {
+        (a, b) if a == b => Outcome::Draw,
+        (Action::Paper, Action::Rock)
+        | (Action::Scissors, Action::Paper)
+        | (Action::Rock, Action::Scissors) => Outcome::Win,
+        _ => Outcome::Loss,
+    }
+}
+
+fn calculate_round_score(me: Action, opponent: Action) -> u32 {
+    me.u32() + determine_round_outcome(me, opponent).u32()
+}
+
 impl StrategyGuide {
+    /// Calculates the score of each action in the strategy guide.
+    ///
+    /// The score is equal to a fixed amount for the shape you played (1 for Rock, 2 for Paper and 3 for Scissors), plus an additional amount based on the outcome of the round; 0 for a loss, 3 for a draw and 6 for a win.
     fn calculate_score(&self) -> u32 {
-        0
+        self.actions.iter().fold(0, |score, (opponent, me)| {
+            score + calculate_round_score(*me, *opponent)
+        })
     }
 }
 
 #[derive(Debug)]
 struct UnknownActionError;
 
-#[derive(Eq, PartialEq, Debug)]
+#[derive(Eq, PartialEq, Debug, Copy, Clone)]
 enum Action {
-    Rock,
-    Paper,
-    Scissors,
+    Rock = 1,
+    Paper = 2,
+    Scissors = 3,
+}
+
+impl Action {
+    fn u32(&self) -> u32 {
+        *self as u32
+    }
 }
 
 impl FromStr for Action {
@@ -66,11 +92,52 @@ impl FromStr for Action {
     }
 }
 
+#[derive(Debug, PartialEq, Eq)]
+enum Outcome {
+    Win = 6,
+    Draw = 3,
+    Loss = 0,
+}
+
+impl Outcome {
+    fn u32(&self) -> u32 {
+        *self as u32
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::day2::StrategyGuide;
+    use crate::day2::{determine_round_outcome, Action, Outcome, StrategyGuide};
 
-    use super::Action;
+    #[test]
+    fn has_correct_scores() {
+        assert_eq!(Action::Rock.u32(), 1);
+        assert_eq!(Action::Paper.u32(), 2);
+        assert_eq!(Action::Scissors.u32(), 3);
+        assert_eq!(Outcome::Win.u32(), 6);
+        assert_eq!(Outcome::Draw.u32(), 3);
+        assert_eq!(Outcome::Loss.u32(), 0);
+
+        assert_eq!(
+            determine_round_outcome(Action::Paper, Action::Rock),
+            Outcome::Win
+        );
+
+        assert_eq!(
+            determine_round_outcome(Action::Rock, Action::Paper),
+            Outcome::Loss
+        );
+
+        assert_eq!(
+            determine_round_outcome(Action::Scissors, Action::Paper),
+            Outcome::Win
+        );
+
+        assert_eq!(
+            determine_round_outcome(Action::Paper, Action::Scissors),
+            Outcome::Loss
+        );
+    }
 
     #[test]
     fn can_parse_steps() {
