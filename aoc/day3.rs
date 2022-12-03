bba74860f7d995094316a12cb459561162f33e7d
Dan <trinitroglycerin@protonmail.com>
2022-12-03 15:09:35 -0800

    Day 3 Part 2

--- aoc/day3.rs
+++ aoc/day3.rs
@@ -31,13 +31,45 @@
 /// In the above example, the priority of the item type that appears in both compartments of each rucksack is 16 (p), 38 (L), 42 (P), 22 (v), 20 (t), and 19 (s); the sum of these is 157.
 ///
 /// Find the item type that appears in both compartments of each rucksack. What is the sum of the priorities of those item types?
+///
+/// --- Part Two ---
+///
+/// As you finish identifying the misplaced items, the Elves come to you with another issue.
+///
+/// For safety, the Elves are divided into groups of three. Every Elf carries a badge that identifies their group. For efficiency, within each group of three Elves, the badge is the only item type carried by all three Elves. That is, if a group's badge is item type B, then all three Elves will have item type B somewhere in their rucksack, and at most two of the Elves will be carrying any other item type.
+///
+/// The problem is that someone forgot to put this year's updated authenticity sticker on the badges. All of the badges need to be pulled out of the rucksacks so the new authenticity stickers can be attached.
+///
+/// Additionally, nobody wrote down which item type corresponds to each group's badges. The only way to tell which item type is the right one is by finding the one item type that is common between all three Elves in each group.
+///
+/// Every set of three lines in your list corresponds to a single group, but each group can have a different badge item type. So, in the above example, the first group's rucksacks are the first three lines:
+///
+/// 	vJrwpWtwJgWrhcsFMMfFFhFp
+/// 	jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+/// 	PmmdzqPrVvPwwTWBwg
+///
+/// And the second group's rucksacks are the next three lines:
+///
+///     wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+///     ttgJtRGJQctTZtZT
+///     CrZsJsPPZsGzwwsLwLmpwMDw
+///
+/// In the first group, the only item type that appears in all three rucksacks is lowercase r; this must be their badges. In the second group, their badge item type must be Z.
+///
+/// Priorities for these items must still be found to organize the sticker attachment efforts: here, they are 18 (r) for the first group and 52 (Z) for the second group. The sum of these is 70.
+///
+/// Find the item type that corresponds to the badges of each three-Elf group. What is the sum of the priorities of those item types?
 use aoc2022::{Part, Result};
-use std::io::{BufRead, BufReader, Read};
+use itertools::Itertools;
+use std::{
+    collections::HashMap,
+    io::{BufRead, BufReader, Read},
+};
 
 pub(crate) fn execute<R: Read>(part: &Part, reader: R) -> Result<()> {
     match part {
-        Part::Part1 => println!("{}", solve(reader)?),
-        _ => unimplemented!(),
+        Part::Part1 => println!("{}", solve(reader)),
+        Part::Part2 => println!("{}", solve2_with_index(reader)),
     };
 
     Ok(())
@@ -46,16 +78,16 @@ pub(crate) fn execute<R: Read>(part: &Part, reader: R) -> Result<()> {
 #[derive(Debug)]
 struct ErrControlCharacter;
 
-fn priority(ch: char) -> std::result::Result<u8, ErrControlCharacter> {
+fn priority(ch: char) -> std::result::Result<u32, ErrControlCharacter> {
     match ch as u8 {
-        c if c > 96 => Ok(c - 96),
+        c if c > 96 => Ok(c as u32 - 96),
         // Upper case letters need to have 27 added, because the minimum priority they can be is 27
-        c if c < 91 => Ok(c - 65 + 27),
+        c if c < 91 => Ok(c as u32 - 65 + 27),
         _ => Err(ErrControlCharacter {}),
     }
 }
 
-fn solve<R: Read>(reader: R) -> Result<u32> {
+fn solve<R: Read>(reader: R) -> u32 {
     let buf = BufReader::new(reader);
     let res = buf.lines().fold(0u32, |acc, line| {
         let line = line.unwrap();
@@ -73,15 +105,43 @@ fn solve<R: Read>(reader: R) -> Result<u32> {
             }
         }
 
-        acc + priority(shared.unwrap()).unwrap() as u32
+        acc + priority(shared.unwrap()).unwrap()
     });
 
-    Ok(res)
+    res
+}
+
+fn solve2_with_index<R: Read>(reader: R) -> u32 {
+    // Another way to solve the Part 2 problem is to build an index of all characters used in each group, and count which char shows up exactly 3 times.
+    let buf = BufReader::new(reader);
+    buf.lines()
+        .filter_ok(|line| !line.is_empty())
+        .chunks(3)
+        .into_iter()
+        .fold(0, |acc, group| {
+            let lines: Vec<Vec<char>> = group
+                .map(|line| line.expect("could not unwrap line").chars().collect())
+                .collect();
+
+            let index = lines.iter().fold(HashMap::new(), |mut index, line| {
+                for c in line.iter().unique() {
+                    index.entry(*c).and_modify(|n| *n += 1u32).or_insert(1);
+                }
+
+                index
+            });
+
+            acc + index
+                .iter()
+                .find_map(|(k, v)| if *v == 3 { Some(k) } else { None })
+                .map(|ch| priority(*ch).unwrap())
+                .unwrap()
+        })
 }
 
 #[cfg(test)]
 mod tests {
-    use super::solve;
+    use super::{solve, solve2_with_index};
 
     const SAMPLE_DATA: &[u8] = b"
 vJrwpWtwJgWrhcsFMMfFFhFp
@@ -93,6 +153,11 @@ CrZsJsPPZsGzwwsLwLmpwMDw";
 
     #[test]
     fn solve_gives_correct_answer_for_sample_data() {
-        assert_eq!(Ok(157), solve(SAMPLE_DATA));
+        assert_eq!(157, solve(SAMPLE_DATA));
+    }
+
+    #[test]
+    fn solve2_with_index_gives_correct_answer_for_sample_data() {
+        assert_eq!(70, solve2_with_index(SAMPLE_DATA));
     }
 }
